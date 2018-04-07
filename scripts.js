8bb4590e1f80249ce9387f2a408dc342f2635a33
IanHinden <Ian.Hinden@gmail.com>
2018-04-07 01:26:07 -0700

    Closed all image tags, Created effect to switch image with video sync

--- scripts.js
+++ scripts.js
@@ -13,6 +13,7 @@
 		//code to check video time repeatedly
 		var tid = setInterval(backgroundChange, 1000);
 		var tid2 = setInterval(shake, 1000);
+		var tid3 = setInterval(changeText, 1000);
 
 		//effect to change background when video reaches certain points
 		function backgroundChange(){ 
@@ -37,9 +38,25 @@
 					if(paused==false){
 						//$("#mainbody").effect("shake",{times:100, distance:10});
 						$(".iframe-scrolled").css({"right":"auto", "left":"auto", "height":"400px"});
+						$("#Ron").attr("src", "images/Sharon.jpg");
 					} else {
-						$(".iframe-scrolled").css({"right":"10", "left":"25%", "height":"200px"});
+						$(".iframe-scrolled").css({"right":"10", "left":"25%", "height":"273px"});
 					}
+				});
+			}
+		})};
+		
+		function changeText(){
+			player.getCurrentTime().then(function(seconds){
+				if(seconds>20){
+					player.getPaused().then(function(paused){
+						if(paused==false){
+							$("#opptext").css("display","none");
+							$("#opptext-hidden").css("display","block");
+						} else {
+							$("#opptext").css("display","block");
+							$("#opptext-hidden").css("display","none");
+						}
 				});
 			}
 		})};
